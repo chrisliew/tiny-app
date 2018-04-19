d5e6e6066d34ba6a85d69e0c93a005f091463472
MachoDhogo <chriswvliew@gmail.com>
2018-04-19 22:33:07 +0000

    Finished task 4 W2D4

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var cookieParser = require('cookie-parser');
+
+//this tells Express to use EJS as it's templating engine
 var app = express();
+app.set("view engine", "ejs");
+
 var PORT = process.env.PORT || 8080; // default port 8080
 
 app.use(cookieParser());
@@ -11,6 +15,7 @@ app.use((req, res, next) => {
   next();
 });
 
+//Generates a random 6 digit number for the shortURL.
 function generateRandomString() {
 
   var text = "";
@@ -21,9 +26,6 @@ function generateRandomString() {
   }
   return text;
 }
-// generateRandomString()
-
-app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -46,15 +48,14 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//Creates a new route urls and uses res.render to pass URL data to urls_index.ejs
 app.get("/urls", (req, res) => {
-
-  let templateVars = {
-    urls: urlDatabase
-   };
-
-  res.render("urls_index", templateVars)
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
 });
 
+
+//Creates a new GET route urls/new to present form to user. This will render the page with the form.
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -64,9 +65,9 @@ app.get("/login", (req, res) => {
 })
 
 
-
+//** Very important to have all get requests for urls/xxx above this, so it's from most specific to least.
+// Creates a new route /urls/:id and uses res.render to pass shortURL and longURL data to urls_show.ejs
 app.get("/urls/:id", (req, res) => {
-
   let templateVars = {
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id],
@@ -75,71 +76,102 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//Body parser library allows us to access POST request parameters, where we'll store in urlDatabase for now.
+//Parses the app.post(urls) below to an object for example.
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// This route will match the POST request from urls_new.ejs and handles it.
+// After receive request (in urls/new) and the form is submitted, this adds a new key value pair
+// (ShortURL and LongURL) and creates a new key and value in the database.  Redirects to the new page with ShortURL tag
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-//create new data here?
-//push new data to database?
-//redirect below?
-//redirect does 3 things:
-  // sets http status to 300 level
-  // sets location header to path for redirect
-  // sends repsonse back
-// req.params gives you an object that specifies whatever at end as a key
-// see find function, to iterate through array.
-// write out steps here
-// let x = req.params.whatever
-  //add a new key value pair to URL Database
-  let templateVars = {
-    urls: urlDatabase
-  }
-
-  let random = generateRandomString()
+  let templateVars = {urls: urlDatabase};
+  let random = generateRandomString();
   urlDatabase[random] = req.body.longURL;
-
   res.redirect('/urls/' + random);
 });
 
+//With any URL that has /u/ with corresponding shortURL in the database, this will redirect to the longURL
 app.get("/u/:shortURL", (req, res) => {
-
     var longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+//This matches the POST form in url_index. If user clicks it will delete the line.
 app.post("/urls/:id/delete", (req, res) => {
-    //1. Find URL in database
-    // let urlID = req.params.id;
-
-    delete urlDatabase[req.params.id];
-
+  delete urlDatabase[req.params.id];
   res.redirect("/urls");
 })
 
+//App.Post so the order doesn't matter.  When form at urls_show is clicked, looks at the long URL.
+//Uses BODY below to pull data from the urls_show that entered in then updates the longURL (posts to it)
 app.post("/urls/:id", (req, res) => {
   // 1. Find longURL in database
   let findURL = req.params.id;
-  // 2. Update it
+  // 2. Updates it
   urlDatabase[findURL] = req.body.updatedLongURL;
-
-
   res.redirect("/urls");
 
 });
 
+//This will take the form data from login.ejs. SETS the cookie using res.cookie.
 app.post("/login", (req, res) =>  {
-  // const username = req.body['username'];
   res.cookie("username", req.body['username']);
   res.redirect("/urls");
 });
 
-
+//If the button logout is clicked this will execute.
 app.post("/logout", (req, res) => {
-
   res.clearCookie("username", req.body['username']);
   res.redirect("/urls");
 });
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+  "user3RandomID": {
+    id: "hulkhogan",
+    email: "gawker101@gmail.com",
+    password: "brother"
+  },
+  "user4RandomID": {
+    id: "patrickkane",
+    email: "blackhawks@gmail.com",
+    password: "stanley"
+  }
+};
+
+app.get("/register", (req, res) => {
+  res.render("register");
+})
+
+// Adds a new user object in the global users object which keeps track of the newly
+// registered user's email, password and user ID.  Generate Random User ID. Adds to user database.
+app.post("/register", (req, res) => {
+  let randomID = generateRandomString();
+  let newEmail = req.body.email;
+  let newPassword = req.body.password;
+  users[randomID] = {
+    id: randomID,
+    email: newEmail,
+    password: newPassword
+  };
+  res.cookie("user_id", randomID);
+
+  console.log(users);
+  res.redirect("/urls");
+
+});
+
+
 
 
 
